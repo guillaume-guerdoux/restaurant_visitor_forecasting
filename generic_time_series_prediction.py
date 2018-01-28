28c841eb30d709d2f0cce54dc7d07be0e811537a
Guillaume <guillaume.guerdoux@hotmail.fr>
2018-01-28 13:05:06 +0100

    Use One step windows lstm to predict passenger numbers with LSTM

--- generic_time_series_prediction.py
+++ generic_time_series_prediction.py
@@ -1,53 +1,67 @@
-#import keras
 import pandas as pd
-import numpy as np
-import matplotlib
 import matplotlib.pyplot as plt
-import random
-from keras.layers import Dense, Activation, LSTM
+import numpy as np
+import math
 from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+#  To predict generic time series : https://machinelearningmastery.com/time-series-prediction-lstm-recurrent-neural-networks-python-keras/
 
-# Create random time series between 2000 and 2018
-random.seed(111)
-date = pd.date_range(start='2000', periods=209, freq="M")
-ts = pd.Series(np.random.uniform(-10, 10, size=len(date)), date).cumsum()
-ts.plot()
-#plt.show()
+df = pd.read_csv('data/international-airline-passengers.csv',
+                      usecols=[1])
+print(df.info())
+df.columns = ['passengers']
+df = df/max(df['passengers'])
+dataset = df.values
+dataset = dataset.astype('float32')
 
-TS = np.array(ts)
+'''plt.plot(dataset)
+plt.show()'''
+#print(dataset)
 
-num_periods = 20
-f_horizon = 1
-
-print(TS.shape)
-x_data = TS[:(len(TS) - (len(TS) % num_periods))]
-x_batches = x_data.reshape(-1, 20, 1)
-
-y_data = TS[1:(len(TS) - (len(TS) % num_periods)) + f_horizon]
-y_batches = y_data.reshape(-1, 20, 1)
-
-print(len(x_batches))
-print(x_batches.shape)
-print(x_batches[0:2])
-
-print(y_batches.shape)
-print(y_batches[0:1])
+# split into train and test sets
+train_size = int(len(dataset) * 0.67)
+test_size = len(dataset) - train_size
+train, test = dataset[0:train_size, :], dataset[train_size:len(dataset), :]
+print(len(train), len(test))
 
 
-def test_data(series, forecast, num_periods):
-    test_x_setup = TS[-(num_periods + forecast):]
-    testX = test_x_setup[:num_periods].reshape(-1, 20, 1)
-    testY = TS[-(num_periods):].reshape(-1 , 20, 1)
-    return testX, testY
+# Create X and Y (Y is just the t+1 value than X)
+def create_dataset(dataset, look_back):
+    x_data, y_data = [], []
+    for i in range(len(dataset) - look_back):
+        a = dataset[i:(i+look_back), 0]
+        x_data.append(a)
+        y_data.append(dataset[i + look_back, 0])
+    return np.array(x_data), np.array(y_data)
 
+look_back = 1
+x_train, y_train = create_dataset(train, look_back)
+x_test, y_test = create_dataset(test, look_back)
 
-X_test, Y_test = test_data(TS, f_horizon, num_periods)
-print(X_test.shape)
-print(X_test)
-
+x_train = np.reshape(x_train, (x_train.shape[0], 1, 1))
+x_test = np.reshape(x_test, (x_test.shape[0], 1, 1))
 
 model = Sequential()
-model.add(LSTM(4, input_shape=(20, 1)))
+model.add(LSTM(4, input_shape=(1, look_back)))
 model.add(Dense(1))
-model.compile(loss='mean_squared_error', optimizer='adam')
-model.fit(x_batches, y_batches, epochs=1, batch_size=20)
+model.compile(loss="mean_squared_error", optimizer="adam", metrics=["accuracy"])
+model.fit(x_train, y_train, epochs=30, batch_size=1, verbose=2)
+
+# make predictions
+trainPredict = model.predict(x_train)
+testPredict = model.predict(x_test)
+
+# shift train predictions for plotting
+trainPredictPlot = np.empty_like(dataset)
+trainPredictPlot[:, :] = np.nan
+trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+# shift test predictions for plotting
+testPredictPlot = np.empty_like(dataset)
+testPredictPlot[:, :] = np.nan
+testPredictPlot[len(trainPredict)+(look_back*2):len(dataset), :] = testPredict
+# plot baseline and predictions
+plt.plot(dataset)
+plt.plot(trainPredictPlot)
+plt.plot(testPredictPlot)
+plt.show()
